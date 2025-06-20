f176b8899d1f3426fc88e5e3c75aadb7b7738c34
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2025-06-20 01:14:57 -0300

    Validação dos campos de cadastro

--- src/screens/Cadastro.js
+++ src/screens/Cadastro.js
@@ -1,36 +1,89 @@
-import { useFonts } from "expo-font";
-import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 import {
-  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
   StyleSheet,
   Text,
-  View,
   TextInput,
-  ScrollView,
+  TouchableOpacity,
+  View,
 } from "react-native";
-import { Button, HelperText } from "react-native-paper";
-import Logo from "../components/Logo.js";
-import { useState } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
+import { Button, HelperText } from "react-native-paper";
 import instance from "../axios.js";
+import Logo from "../components/Logo.js";
 
 export default function Cadastro({ navigation }) {
   async function cadastrarUsuario() {
-    const dataNascimento = new Date();
-
-    await instance.post("/cadastro", {
-      nome,
-      sobrenome,
-      usuario,
-      genero,
-      dataNascimento,
-      email,
-      senha,
-    });
+    setHelperGenero(true);
+    if (!hasErrors()) {
+      const dataNascimento = new Date(Number(ano), Number(mes), Number(dia));
+      instance.post("/cadastro", {
+        nome,
+        sobrenome,
+        usuario,
+        genero,
+        dataNascimento,
+        email,
+        senha,
+      }).then((response)=>{
+        
+      });
+    }
   }
 
-   const hasErrors = () => {
-    return !email.includes('@');
+  const hasErrorsTexto = (text) => {
+    return text.trim().length < 3;
+  };
+
+  const hasErrorsEmail = () => {
+    return !email.includes("@") || email.trim().length < 10;
+  };
+
+  const hasErrorsDataDeNascimento = () => {
+    return (
+      Number(ano) < 1900 ||
+      Number(ano) > new Date().getFullYear() ||
+      Number(mes) < 1 ||
+      Number(mes) > 12 ||
+      Number(dia) < 1 ||
+      (Number(dia) > 31 && [1, 3, 5, 7, 8, 10, 12].includes(Number(mes))) ||
+      ([4, 6, 9, 11].includes(Number(mes)) && Number(dia) > 30) ||
+      (Number(dia) > 29 && Number(mes) === 2)
+    );
+  };
+
+  const hasErrorsGenero = () => {
+    return genero == null;
+  };
+
+  const hasErrorsSenha = () => {
+    return (
+      senha.match(/[0-9]/g) == null ||
+      senha.match(/[A-Z]/g) == null ||
+      senha.match(/[a-z]/g) == null ||
+      senha.match(/[\W|_]/g) == null ||
+      senha.trim().length < 8
+    );
+  };
+
+  const hasErrorsConfirmarSenha = () => {
+    return senha != confirmaSenha;
+  };
+
+  const hasErrors = () => {
+    return (
+      hasErrorsTexto(nome) ||
+      hasErrorsTexto(sobrenome) ||
+      hasErrorsEmail() ||
+      hasErrorsTexto(usuario) ||
+      hasErrorsDataDeNascimento() ||
+      hasErrorsSenha() ||
+      hasErrorsConfirmaSenha() ||
+      hasErrorsGenero
+    );
   };
 
   const [open, setOpen] = useState(false);
@@ -49,119 +102,270 @@ export default function Cadastro({ navigation }) {
   const [dia, setDia] = useState("");
   const [mes, setMes] = useState("");
   const [ano, setAno] = useState("");
+  const [helperEmail, setHelperEmail] = useState(false);
+  const [helperNome, setHelperNome] = useState(false);
+  const [helperUsuario, setHelperUsuario] = useState(false);
+  const [helperSobrenome, setHelperSobrenome] = useState(false);
+  const [helperSenha, setHelperSenha] = useState(false);
+  const [helperConfirmarSenha, setHelperConfirmarSenha] = useState(false);
+  const [helperDataDeNascimento, setHelperDataDeNascimento] = useState(false);
+  const [helperGenero, setHelperGenero] = useState(false);
+  const [showSenha, setShowSenha] = useState(false);
+  const [showConfirmarSenha, setShowConfirmarSenha] = useState(false);
   return (
     <View style={styles.container}>
       <Logo />
       <View style={{ margin: 20, height: "80%" }}>
-        <ScrollView>
-          <Text style={{ alignSelf: "center", fontSize: 19 }}>
-            Dados Pessoais
-          </Text>
-          <View
-            style={{
-              height: 1,
-              backgroundColor: "#000",
-              width: 100,
-              alignSelf: "center",
-              marginVertical: 5,
-            }}
-          />
-          <Text style={styles.label}>Nome:</Text>
-          <TextInput style={styles.input} onChangeText={setNome}></TextInput>
-          <Text style={styles.label}>Sobrenome:</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={setSobrenome}
-          ></TextInput>
-          <Text style={styles.label}>Data de Nascimento:</Text>
-          <View style={{ flexDirection: "row", gap: 10 }}>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 120}
+        >
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            keyboardShouldPersistTaps="handled"
+          >
+            <Text style={{ alignSelf: "center", fontSize: 19 }}>
+              Dados Pessoais
+            </Text>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: "#000",
+                width: 100,
+                alignSelf: "center",
+                marginVertical: 5,
+              }}
+            />
+            <Text style={styles.label}>Nome:</Text>
             <TextInput
-              style={{ ...styles.input, width: 50, textAlign: "center" }}
-              placeholder="DD"
-              onChangeText={setDia}
+              style={styles.input}
+              onChangeText={(text) => {
+                setNome(text);
+                setHelperNome(true);
+              }}
             ></TextInput>
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsTexto(nome) && helperNome}
+            >
+              Preencha seu nome
+            </HelperText>
+            <Text style={styles.label}>Sobrenome:</Text>
+
             <TextInput
-              style={{ ...styles.input, width: 50, textAlign: "center" }}
-              placeholder="MM"
-              onChangeText={setMes}
+              style={styles.input}
+              onChangeText={(text) => {
+                setSobrenome(text);
+                setHelperSobrenome(true);
+              }}
             ></TextInput>
+
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsTexto(sobrenome) && helperSobrenome}
+            >
+              Preencha seu sobrenome
+            </HelperText>
+            <Text style={styles.label}>Data de Nascimento:</Text>
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <TextInput
+                style={{ ...styles.input, width: 50, textAlign: "center" }}
+                placeholder="DD"
+                onChangeText={(text) => {
+                  setHelperDataDeNascimento(true);
+                  setDia(text);
+                }}
+              ></TextInput>
+
+              <TextInput
+                style={{ ...styles.input, width: 50, textAlign: "center" }}
+                placeholder="MM"
+                onChangeText={(text) => {
+                  setHelperDataDeNascimento(true);
+                  setMes(text);
+                }}
+              ></TextInput>
+
+              <TextInput
+                style={{ ...styles.input, width: 80, textAlign: "center" }}
+                placeholder="AAAA"
+                onChangeText={(text) => {
+                  setHelperDataDeNascimento(true);
+                  setAno(text);
+                }}
+              ></TextInput>
+            </View>
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsDataDeNascimento() && helperDataDeNascimento}
+            >
+              Preencha uma data de nascimento válida
+            </HelperText>
+            <Text style={styles.label}>Gênero:</Text>
+            <DropDownPicker
+              style={styles.input}
+              open={open}
+              value={genero}
+              items={generos}
+              setOpen={setOpen}
+              setValue={setGenero}
+              setItems={setGeneros}
+              placeholder="Selecionar"
+              listMode="SCROLLVIEW"
+              dropDownContainerStyle={{
+                backgroundColor: "none",
+                borderWidth: 0,
+                borderRadius: 20,
+                width: 300,
+                marginHorizontal: 5,
+              }}
+              // Estilo de cada item da lista
+              listItemContainerStyle={{
+                backgroundColor: "none",
+                borderWidth: 0,
+                borderRadius: 20,
+                width: 300,
+                height: 45,
+              }}
+              // Estilo do texto de cada item
+              listItemLabelStyle={{
+                color: "#333",
+                fontSize: 14,
+              }}
+              selectedItemLabelStyle={{
+                color: "#6200ee",
+                fontWeight: "bold",
+              }}
+              // Estilo do placeholder
+              placeholderStyle={{
+                color: "grey",
+              }}
+              // Estilo da seta
+              arrowIconStyle={{
+                tintColor: "#6200ee",
+              }}
+            />
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsGenero() && helperGenero}
+            >
+              Preencha um gênero
+            </HelperText>
+            <Text style={{ alignSelf: "center", fontSize: 19, marginTop: 20 }}>
+              Dados Cadastrais
+            </Text>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: "#000",
+                width: 100,
+                alignSelf: "center",
+                marginVertical: 5,
+              }}
+            />
+            <Text style={styles.label}>Email:</Text>
+
             <TextInput
-              style={{ ...styles.input, width: 80, textAlign: "center" }}
-              placeholder="AAAA"
-              onChangeText={setAno}
+              style={styles.input}
+              onChangeText={(text) => {
+                setEmail(text);
+                setHelperEmail(true);
+              }}
             ></TextInput>
-          </View>
-          <Text style={styles.label}>Gênero:</Text>
-          <DropDownPicker
-            style={styles.input}
-            open={open}
-            value={genero}
-            items={generos}
-            setOpen={setOpen}
-            setValue={setGenero}
-            setItems={setGeneros}
-            placeholder="Selecionar"
-            listMode="SCROLLVIEW"
-            dropDownContainerStyle={{
-              backgroundColor: "none",
-              borderWidth: 0,
-              borderRadius: 20,
-              width: 200,
-              marginHorizontal: 5,
-            }}
-            // Estilo de cada item da lista
-            listItemContainerStyle={{
-              backgroundColor: "none",
-              borderWidth: 0,
-              borderRadius: 20,
-              width: 200,
-              height: 45,
-            }}
-            // Estilo do texto de cada item
-            listItemLabelStyle={{
-              color: "#333",
-              fontSize: 14,
-            }}
-            selectedItemLabelStyle={{
-              color: "#6200ee",
-              fontWeight: "bold",
-            }}
-            // Estilo do placeholder
-            placeholderStyle={{
-              color: "grey",
-            }}
-            // Estilo da seta
-            arrowIconStyle={{
-              tintColor: "#6200ee",
-            }}
-          />
-          <Text style={{ alignSelf: "center", fontSize: 19, marginTop: 20 }}>
-            Dados Cadastrais
-          </Text>
-          <View
-            style={{
-              height: 1,
-              backgroundColor: "#000",
-              width: 100,
-              alignSelf: "center",
-              marginVertical: 5,
-            }}
-          />
-          <Text style={styles.label}>Email:</Text>
-          <TextInput style={styles.input} onChangeText={setEmail}></TextInput>
-          <HelperText type="error" visible={hasErrors()}>
-            Email address is invalid!
-          </HelperText>
-          <Text style={styles.label}>Usuário:</Text>
-          <TextInput style={styles.input} onChangeText={setUsuario}></TextInput>
-          <Text style={styles.label}>Senha:</Text>
-          <TextInput style={styles.input} onChangeText={setSenha}></TextInput>
-          <Text style={styles.label}>Confirmar Senha:</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={setConfirmaSenha}
-          ></TextInput>
-        </ScrollView>
+
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsEmail() && helperEmail}
+            >
+              Preencha um endereço de email válido
+            </HelperText>
+            <Text style={styles.label}>Usuário:</Text>
+
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => {
+                setUsuario(text);
+                setHelperUsuario(true);
+              }}
+            ></TextInput>
+
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsTexto(usuario) && helperUsuario}
+            >
+              Preencha um nome de usuário válido
+            </HelperText>
+
+            <Text style={styles.label}>Senha:</Text>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <TextInput
+                style={styles.input}
+                onChangeText={(text) => {
+                  setSenha(text);
+                  setHelperSenha(true);
+                }}
+                secureTextEntry={!showSenha}
+              ></TextInput>
+              <TouchableOpacity
+                onPress={() => setShowSenha(!showSenha)}
+                style={{ marginLeft: -50 }}
+              >
+                <Ionicons
+                  name={showSenha ? "eye" : "eye-off"}
+                  size={24}
+                  color="gray"
+                />
+              </TouchableOpacity>
+            </View>
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={hasErrorsSenha() && helperSenha}
+            >
+              {!hasErrorsSenha() || !helperSenha
+                ? " "
+                : "A senha precisa conter:\n • 8 caracteres\n • 1 letra maiúscula\n • 1 número\n • 1 caractere especia"}
+              l
+            </HelperText>
+            <Text style={styles.label}>Confirmar Senha:</Text>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <TextInput
+                style={styles.input}
+                onChangeText={(text) => {
+                  setConfirmaSenha(text);
+                  setHelperConfirmarSenha(true);
+                }}
+                secureTextEntry={true}
+              ></TextInput>
+              <TouchableOpacity
+                onPress={() => setShowConfirmarSenha(!showConfirmarSenha)}
+                style={{ marginLeft: -50 }}
+              >
+                <Ionicons
+                  name={showConfirmarSenha ? "eye" : "eye-off"}
+                  size={24}
+                  color="gray"
+                />
+              </TouchableOpacity>
+            </View>
+            <HelperText
+              style={styles.helper}
+              type="error"
+              visible={
+                hasErrorsConfirmarSenha(confirmaSenha) && helperConfirmarSenha
+              }
+            >
+              A senha não é igual
+            </HelperText>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </View>
       <View style={{ flexDirection: "row", justifyContent: "center", gap: 35 }}>
         <Button
@@ -177,7 +381,7 @@ export default function Cadastro({ navigation }) {
           mode="elevated"
           textColor="white"
           buttonColor="#6446db"
-          style={{ width: 150 }}
+          style={{ width: 150, opacity: hasErrors() ? 0.4 : 1 }}
           onPress={cadastrarUsuario}
         >
           Salvar
@@ -195,14 +399,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#EEEEEE",
   },
   input: {
-    width: 250,
+    width: 300,
     height: 45,
     backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 10,
     borderRadius: 20,
-    marginVertical: 10,
+    marginTop: 5,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -216,5 +420,9 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 18,
+  },
+  helper: {
+    fontSize: 13,
+    textColor: "red",
   },
 });
